f8949a95f5e63d018dc1473e65ffcf5f0bc1482f
wendysoza <calle-wendy3995@unesum.edu.ec>
2026-07-06 15:13:36 -0500

    cambios de contenido

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -67,7 +67,7 @@ export default function LandingPage({ onNavigateToAuth }) {
             <GraduationCap className="w-4 h-4" /> Proyecto de Titulación UNESUM
           </div>
           <h1 className="text-5xl lg:text-7xl font-black text-white leading-tight tracking-tight mb-8 drop-shadow-2xl">
-            Gamificación para las <br/>
+            Juegos interactivos para las <br/>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-400">
               Tecnologías Emergentes
             </span>
